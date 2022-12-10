03e5e8856bf1f72cccb02ffbd1af6e9bdddcda62
Hossein <djalicrt@gmail.com>
2022-12-10 18:16:18 +0330

    Create Adapter Package and Start to impl. it

--- app/src/main/java/ir/shariaty/fifaworldcup2/MainActivity.java
+++ app/src/main/java/ir/shariaty/fifaworldcup2/MainActivity.java
@@ -5,8 +5,10 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import ir.shariaty.fifaworldcup2.manager.RequestManager;
+import ir.shariaty.fifaworldcup2.models.FixtureResponse;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerview;
@@ -24,6 +26,21 @@ public class MainActivity extends AppCompatActivity {
         dialog.setTitle("Loading...⌛");
 
         manager = new RequestManager(this);
+        // TODO I'll fix it later.
         manager.getFixture();
     }
+
+    private final ResponseListener listener = new ResponseListener() {
+
+        @Override
+        public void didFetch(FixtureResponse response, String status) {
+            dialog.dismiss();
+        }
+
+        @Override
+        public void didError(String status) {
+            dialog.dismiss();
+            Toast.makeText(MainActivity.this, status, Toast.LENGTH_SHORT).show();
+        }
+    };
 }
---
+++ app/src/main/java/ir/shariaty/fifaworldcup2/adapter/FixtureAdapter.java
@@ -0,0 +1,4 @@
+package ir.shariaty.fifaworldcup2.adapter;
+
+public class FixtureAdapter {
+}
